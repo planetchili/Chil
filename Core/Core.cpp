5ba2b77385fa91fde141e5420848dbcbea4eb462
Chili <planetchili@users.noreply.github.com>
2022-12-25 17:58:10 +0900

    static analysis

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,7 +1,21 @@
 // Core.cpp : Defines the functions for the static library.
 //
+#include <memory>
+
+struct Base
+{
+	virtual int Test() { return 420; }
+	virtual ~Base() = default;
+};
+
+struct Derived : public Base
+{
+	int Test() override { return 69; }
+};
+
+std::unique_ptr<Base> MakeDerived() { return std::make_unique<Derived>(); }
 
 int Test()
 {
-	return 420;
+	return MakeDerived()->Test();
 }
