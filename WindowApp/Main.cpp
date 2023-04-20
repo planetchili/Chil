5daa49cf39c03681b664f1a688e773a2424db021
Chili <planetchili@users.noreply.github.com>
2023-04-20 10:38:45 +0900

    project for winmain

---
+++ WindowApp/Main.cpp
@@ -0,0 +1,31 @@
+#include <Core/src/win/ChilWin.h>
+#include <Core/src/ioc/Container.h> 
+#include <Core/src/log/SeverityLevelPolicy.h> 
+#include <Core/src/win/Boot.h>
+#include <Core/src/log/Log.h> 
+
+using namespace chil;
+
+void Boot()
+{
+	log::Boot();
+	ioc::Get().Register<log::ISeverityLevelPolicy>([] {
+		return std::make_shared<log::SeverityLevelPolicy>(log::Level::Info);
+	});
+
+	win::Boot();
+}
+
+int WINAPI wWinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	PWSTR pCmdLine,
+	int nCmdShow)
+{
+	Boot();
+
+	chilog.info(L"Hello from window");
+	MessageBoxA(nullptr, "", "Hi Guise!", MB_APPLMODAL | MB_ICONEXCLAMATION);
+
+	return 0;
+}
